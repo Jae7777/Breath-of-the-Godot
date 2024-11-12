bdbe183cbc01fbe85f92984418a06e2415781872
Justin Chen <jchen4086@gmail.com>
2024-11-12 17:46:44 -0500

    player and camera movement

---
+++ breath-of-the-godot/scenes/levels/camera_control_component.gd
@@ -0,0 +1,19 @@
+extends Node
+
+@export var camera: Node3D
+@export var target: Node3D
+@export var min_limit_x := -0.8
+@export var max_limit_x := -0.2
+@export var mouse_acceleration := 0.005
+
+func _physics_process(_delta: float) -> void:
+	camera.global_transform.origin = target.global_transform.origin
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion:
+		rotate_from_vector(event.relative * mouse_acceleration)
+
+func rotate_from_vector(v: Vector2) -> void:
+	if v.length() == 0: return
+	camera.rotation.y -= v.x
+	camera.rotation.x = clamp(camera.rotation.x + v.y, min_limit_x, max_limit_x)
